737e6c5728a9eda187f1ef1e787932b933379bc6
Sanskriti <sanskriti182004@gmail.com>
2025-09-04 11:36:24 +0530

    hikari and table setup

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,20 +1,26 @@
 package com.example
 
 import com.example.data.DatabaseFactory
+import com.example.data.UserDataSource
 import com.example.security.configureShiprocketSecurity
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import java.util.*
 
 fun main(args: Array<String>) {     //entry point of app
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"))
     io.ktor.server.netty.EngineMain.main(args)      //Netty is a high-performance asynchronous networking framework
 }
 
 fun Application.module() { // main application module - setup code for server what
 
+    println("JVM TZ = ${java.util.TimeZone.getDefault().id}")
+
     val databaseFactory = DatabaseFactory()
-    databaseFactory.database
+//    databaseFactory.database
+    val userDataSource = UserDataSource(databaseFactory.database)
 
     configureSerialization()
     configureShiprocketSecurity()
--- src/main/kotlin/data/DatabaseFactory.kt
+++ src/main/kotlin/data/DatabaseFactory.kt
@@ -1,14 +1,31 @@
 package com.example.data
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 
 class DatabaseFactory {
 
-    val database = Database.connect(
-        url = "jdbc:postgresql://localhost:5432/shopping_db",
-        user = "admin",
-        password = "postgresSecret",
-        driver = "org.postgresql.Driver"
+    private val config = HikariConfig().apply {
+        jdbcUrl = "jdbc:postgresql://localhost:5432/shopping_db"
+        driverClassName = "org.postgresql.Driver"
+        username = "admin"
+        password = "postgresSecret"
+        isReadOnly = false
+        maximumPoolSize = 7
+        transactionIsolation = "TRANSACTION_SERIALIZABLE"
+    }
 
-    )
+    private val dataSource = HikariDataSource(config)
+
+    val database = Database.connect(dataSource)
+
+    //MANUAL DB SETUP W/O HIKARI
+//    val database = Database.connect(
+//        url = "jdbc:postgresql://localhost:5432/shopping_db",
+//        user = "admin",
+//        password = "postgresSecret",
+//        driver = "org.postgresql.Driver"
+//
+//    )
 }
